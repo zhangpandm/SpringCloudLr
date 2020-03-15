d3731cdd4b14d9fe2efec2b2dfa1f2badec3d9c7
zhangpanm <zhangpanm@yonyou.com>
2020-03-15 21:40:30 +0800

    切换负载均衡算法

--- sc-cunsumer-9001/src/main/java/com/zp/lr/service/ZPConfig.java
+++ sc-cunsumer-9001/src/main/java/com/zp/lr/service/ZPConfig.java
@@ -5,15 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration
 public class ZPConfig {
 	
 	@Bean
-	@LoadBalanced
+	@LoadBalanced//ribbon 负载均衡器
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
-    
+    // 配置切换负载均衡规则算法
+	@Bean
+	public IRule getIRule(){
+		return new  RandomRule();
+	}
 }
  
 	
