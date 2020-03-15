97aada30dd03013c47a92aff00a44703b816a4b0
zhangpanm <zhangpanm@yonyou.com>
2020-03-15 21:32:44 +0800

    ribbon loadBalance可以irule切换负载规则

--- sc-cunsumer-9001/src/main/java/com/zp/lr/service/ZPConfig.java
+++ sc-cunsumer-9001/src/main/java/com/zp/lr/service/ZPConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration
 public class ZPConfig {
 	
@@ -13,7 +16,11 @@ public class ZPConfig {
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
-    
+	@Bean
+	public IRule getIRule(){
+		return new RandomRule();
+		
+	}
 }
  
 	
